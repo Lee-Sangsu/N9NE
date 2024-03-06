aa1e799ac6b32413e90aa1bca48ff5fb536a8be5
Lee-Sangsu <sangsulee200@gmail.com>
2024-03-06 12:28:12 +0900

    index page edited

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,43 @@ import ConsultBTN from "@/components/Button/consult_button";
 export default function Home() {
   return (
     <main className={styles.main}>
+        <Image
+          className={styles.star}
+          src="/images/star.svg"
+          alt="Nine decor 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          className={styles.rect}
+          src="/images/rounded-rect.svg"
+          alt="Nine decor 2"
+          width={150}
+          height={300}
+        />
 
       <div className={styles.index_container}>
         <h1>Make Your Brand Memorable</h1>
         <h2>Express your brand identity to connect with your customers</h2>
         <ConsultBTN />
-        {/* <Image
-          className={styles.logo}
-          src="/full-icon.svg"
-          alt="Nine Logo"
-          width={180}
-          height={37}
-          priority
-        /> */}
+      </div>
+
+      <div className={styles.statement}>
+        <h2>We transform We innovate we create</h2>
+        <p>
+        We are N9NE, a creative and dynamic agency made up of a team of designers, communicators, strategists, and leaders.
+        With a collective and multicultural approach, we enhance our design process to adapt it to the unique needs of each client, creating memorable and meaningful brands.
+        We're eager to help you create a brand that reflects your vision and connects with your audience in deep and meaningful ways.
+        </p>
+      </div>
+      
+      <div>
+        <h2>Services</h2>
+        <p>
+        We are N9NE, a creative and dynamic agency made up of a team of designers, communicators, strategists, and leaders.
+        With a collective and multicultural approach, we enhance our design process to adapt it to the unique needs of each client, creating memorable and meaningful brands.
+        We're eager to help you create a brand that reflects your vision and connects with your audience in deep and meaningful ways.
+        </p>
       </div>
 
       <Footer />
